0a672cf41c8c0335130e5e9ab0baf28243081161
lnicosia <lnicosia@student.42.fr>
2018-11-13 18:27:45 +0100

    get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/13 18:23:24 by lnicosia          #+#    #+#             */
+/*   Updated: 2018/11/13 18:25:46 by lnicosia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# define BUFF_SIZE 4096
+
+int	get_next_line(const int fd, char **line);
+
+#endif
