584869398af0fd03e35f3a8bad01d12220a3a4da
lnicosia <lnicosia@student.42.fr>
2018-11-20 17:38:12 +0100

    norme.

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,21 @@
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 11:05:31 by lnicosia          #+#    #+#             */
-/*   Updated: 2018/11/20 16:45:23 by lnicosia         ###   ########.fr       */
+/*   Updated: 2018/11/20 17:36:26 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
 
-int		lst_contains(t_list *lst, t_read **current, int fd)
+int		lst_contains(t_list *lst, t_read **curr, int fd)
 {
 	while (lst != NULL)
 	{
-		*current = (t_read*)(lst->content);
-		if (*current != NULL)
+		*curr = (t_read*)(lst->content);
+		if (*curr != NULL)
 		{
-			if ((*current)->fd == fd)
+			if ((*curr)->fd == fd)
 				return (1);
 		}
 		lst = lst->next;
@@ -41,90 +41,64 @@ char	*strealloc(char *s1, int size1, char *s2, int size2)
 	return (s1);
 }
 
-void		set_data(t_read *current, char **line)
+void	set_data(t_read *curr, char **line)
 {
 	size_t	i;
 	char	*tmp;
 
 	i = 0;
-	//ft_putstr("-TOTAL READ: "); ft_putendl(current->str);
-	while ((current->str[i] != '\n') && (current->str[i]))
+	while ((curr->str[i] != '\n') && (curr->str[i]))
 		i++;
 	*line = ft_strnew(i);
-	ft_strncpy(*line, current->str, i);
-	//ft_putstr("-LINE: "); ft_putendl(*line);
-	if (i < ft_strlen(current->str) - 1)
+	ft_strncpy(*line, curr->str, i);
+	if (i < ft_strlen(curr->str) - 1)
 	{
-		tmp = current->str;
-		current->str = ft_strsub(current->str, i + 1, ft_strlen(current->str) - i - 1);
+		tmp = curr->str;
+		curr->str = ft_strsub(curr->str, i + 1, ft_strlen(curr->str) - i - 1);
 		ft_strdel(&tmp);
 	}
 	else
 	{
-		//ft_putendl("Read just enough.");
-		ft_strdel(&(current->str));
-		current->str = ft_strnew(0);
+		ft_strdel(&(curr->str));
+		curr->str = ft_strnew(0);
 	}
+}
+
+int		set_and_add(t_list **datas, char **line, t_read *curr, int new)
+{
+	set_data(curr, line);
+	if (new == 0)
+	{
+		ft_lstadd(datas, ft_lstnew(curr, sizeof(*curr)));
+		free(curr);
+		curr = NULL;
+	}
+	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	static t_list	*datas = NULL;
-	//t_list			*tmp;
 	int				new;
-	t_read			*current;
+	t_read			*curr;
 	char			buff[BUFF_SIZE + 1];
-	int				bytes;
+	int				ret;
 
-	//ft_putendl("-- NEW CALL --");
 	if (fd < 0 || line == NULL)
 		return (-1);
-	if ((new = lst_contains(datas, &current, fd)) == 0)
+	if ((new = lst_contains(datas, &curr, fd)) == 0)
 	{
-		current = (t_read*)malloc(sizeof(*current));
-		current->str = ft_strnew(0);
-		current->fd = fd;
-		//ft_putstr("-- UNKNOWN FD: "); ft_putnbr(fd); ft_putendl(" --");
+		curr = (t_read*)malloc(sizeof(*curr));
+		curr->str = ft_strnew(0);
+		curr->fd = fd;
 	}
-	else
+	while ((!(ft_strchr(curr->str, '\n'))) && (ret = read(fd, buff, BUFF_SIZE)))
 	{
-		//ft_putstr("-- KNOWN FD: "); ft_putnbr(current->fd); ft_putendl(" --");
-		if (ft_strchr(current->str, '\n') != NULL)
-		{
-			set_data(current, line);
-			return (1);
-		}
-	}
-	while ((bytes = read(fd, buff, BUFF_SIZE)))
-	{
-		if (bytes < 0)
+		if (ret < 0)
 			return (-1);
-		current->str = strealloc(current->str, ft_strlen(current->str), buff, bytes);
-		//ft_putstr("read: "); ft_putendl(current->str);
-		if (ft_strchr(buff, '\n') != NULL)
-		{
-			set_data(current, line);
-			if (new == 0)
-			{
-				ft_lstadd(&datas, ft_lstnew(current, sizeof(*current)));
-				free(current);
-				current = NULL;
-			}
-			return (1);
-		}
+		curr->str = strealloc(curr->str, ft_strlen(curr->str), buff, ret);
 	}
-	if (current->str[0])
-	{
-		//ft_putendl("-EOF: ");
-		set_data(current, line);
-		if (new == 0)
-		{
-			ft_lstadd(&datas, ft_lstnew(current, sizeof(*current)));
-			free(current);
-			current = NULL;
-		}
-		return (1);
-	}
-	//ft_putstr("-- FD "); ft_putnbr(fd); ft_putendl(" read --");
+	if (curr->str[0])
+		return (set_and_add(&datas, line, curr, new));
 	return (0);
 }
---
+++ get_next_line_logs.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/14 11:05:31 by lnicosia          #+#    #+#             */
+/*   Updated: 2018/11/20 16:45:23 by lnicosia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include "libft.h"
+
+int		lst_contains(t_list *lst, t_read **current, int fd)
+{
+	while (lst != NULL)
+	{
+		*current = (t_read*)(lst->content);
+		if (*current != NULL)
+		{
+			if ((*current)->fd == fd)
+				return (1);
+		}
+		lst = lst->next;
+	}
+	return (0);
+}
+
+char	*strealloc(char *s1, int size1, char *s2, int size2)
+{
+	char	*tmp;
+
+	tmp = s1;
+	s2[size2] = '\0';
+	s1 = ft_strnew(size1 + size2);
+	ft_strcpy(s1, tmp);
+	ft_strdel(&tmp);
+	ft_strcat(s1, s2);
+	return (s1);
+}
+
+void		set_data(t_read *current, char **line)
+{
+	size_t	i;
+	char	*tmp;
+
+	i = 0;
+	//ft_putstr("-TOTAL READ: "); ft_putendl(current->str);
+	while ((current->str[i] != '\n') && (current->str[i]))
+		i++;
+	*line = ft_strnew(i);
+	ft_strncpy(*line, current->str, i);
+	//ft_putstr("-LINE: "); ft_putendl(*line);
+	if (i < ft_strlen(current->str) - 1)
+	{
+		tmp = current->str;
+		current->str = ft_strsub(current->str, i + 1, ft_strlen(current->str) - i - 1);
+		ft_strdel(&tmp);
+	}
+	else
+	{
+		//ft_putendl("Read just enough.");
+		ft_strdel(&(current->str));
+		current->str = ft_strnew(0);
+	}
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static t_list	*datas = NULL;
+	//t_list			*tmp;
+	int				new;
+	t_read			*current;
+	char			buff[BUFF_SIZE + 1];
+	int				bytes;
+
+	//ft_putendl("-- NEW CALL --");
+	if (fd < 0 || line == NULL)
+		return (-1);
+	if ((new = lst_contains(datas, &current, fd)) == 0)
+	{
+		current = (t_read*)malloc(sizeof(*current));
+		current->str = ft_strnew(0);
+		current->fd = fd;
+		//ft_putstr("-- UNKNOWN FD: "); ft_putnbr(fd); ft_putendl(" --");
+	}
+	else
+	{
+		//ft_putstr("-- KNOWN FD: "); ft_putnbr(current->fd); ft_putendl(" --");
+		if (ft_strchr(current->str, '\n') != NULL)
+		{
+			set_data(current, line);
+			return (1);
+		}
+	}
+	while ((bytes = read(fd, buff, BUFF_SIZE)))
+	{
+		if (bytes < 0)
+			return (-1);
+		current->str = strealloc(current->str, ft_strlen(current->str), buff, bytes);
+		//ft_putstr("read: "); ft_putendl(current->str);
+		if (ft_strchr(buff, '\n') != NULL)
+		{
+			set_data(current, line);
+			if (new == 0)
+			{
+				ft_lstadd(&datas, ft_lstnew(current, sizeof(*current)));
+				free(current);
+				current = NULL;
+			}
+			return (1);
+		}
+	}
+	if (current->str[0])
+	{
+		//ft_putendl("-EOF: ");
+		set_data(current, line);
+		if (new == 0)
+		{
+			ft_lstadd(&datas, ft_lstnew(current, sizeof(*current)));
+			free(current);
+			current = NULL;
+		}
+		return (1);
+	}
+	//ft_putstr("-- FD "); ft_putnbr(fd); ft_putendl(" read --");
+	return (0);
+}
